10009737fe6ad3ddb8a72ad986ac11c164cbe61d
d87 <d87d87@gmail.com>
2012-10-14 18:55:51 +0700

    pandemic range lowered by 1s

--- config.lua
+++ config.lua
@@ -58,8 +58,10 @@ Spell({2825, 32182, 80353} ,{ name = "Bloodlust", duration = 40, priority = -100
 
 if class == "WARLOCK" then
 Spell( 74434 ,{ name = "Soulburn",duration = 20, color = colors.CURSE })
+--I can't know for sure what base debuff duration was because of haste,
+--so all values are just 1s less than 50% of base duration without haste to be safe
 --Immolate
-Spell( 348 ,{ name = "", recast_mark = 7.5, duration = 15, nameplates = true, priority = 10, ghost = true, color = colors.RED })
+Spell( 348 ,{ name = "", recast_mark = 6.5, duration = 15, nameplates = true, priority = 10, ghost = true, color = colors.RED })
 
 Spell( 34936 ,{ name = "Backlash", duration = 8, shine = true, color = colors.CURSE })
 -- Spell( 117828 ,{ name = "Backdraft", duration = 15, shine = true, color = colors.WOO, charged = true, maxcharge = 3 })
@@ -70,7 +72,7 @@ Cooldown( 17962, { name = "Conflagrate", ghost = true, priority = 5, color = col
 
 Spell( 122355,{ name = "Molten Core",duration = 30, shine = true, color = colors.PURPLE })
 --Doom
-Spell( 603 ,{ name = "", duration = 60, nameplates = true, recast_mark = 30, ghost = true, priority = 6, color = colors.WOO })
+Spell( 603 ,{ name = "", duration = 60, nameplates = true, recast_mark = 29, ghost = true, priority = 6, color = colors.WOO })
 -- REMOVED_DOSE event is not fired for molten core, so it's stuck at 3
 
 Cooldown( 105174, { name = "Hand of Gul'dan",  ghost = true, shinerefresh = true, color = colors.CURSE })
@@ -87,14 +89,14 @@ Spell( 86211 ,{ name = "Soul Swap", duration = 20, shine = true, color = colors.
 Spell( 103103 ,{ name = "Malefic Grasp", priority = 14, duration = 15, color = colors.CURSE, target = "target" })
 
 --Unstable Affliction
-Spell( 30108 ,{ name = "", priority = 10, nameplates = true, duration = 15, ghost = true, recast_mark = 7.5, color = colors.RED })
+Spell( 30108 ,{ name = "", priority = 10, nameplates = true, duration = 15, ghost = true, recast_mark = 6.5, color = colors.RED })
 
 --Haunt
 Spell( 48181 ,{ name = "Haunt",duration = 12, priority = 8, ghost = true, nameplates = true, color = colors.TEAL }) --Haunt
 --Corruption (2nd is a Soulburn SoC Corruption) --87389
-Spell( 172 ,{ name = "", priority = 9, nameplates = true, recast_mark = 9, nameplates = true, ghost = true, color = colors.PINK, duration = 18 })
+Spell( 172 ,{ name = "", priority = 9, nameplates = true, recast_mark = 8, nameplates = true, ghost = true, color = colors.PINK, duration = 18 })
 --Agony
-Spell( 980 ,{ name = "", duration = 24, nameplates = true, recast_mark = 12, ghost = true, priority = 6, color = colors.WOO })
+Spell( 980 ,{ name = "", duration = 24, nameplates = true, recast_mark = 11, ghost = true, priority = 6, color = colors.WOO })
 Spell( 1120 ,{ name = "Drain Soul",duration = 15, color = colors.LRED })
 Spell( 27243 ,{ name = "Seed of Corruption",duration = 18, nameplates = true,  color = colors.LRED, short = "SoC" })
 
